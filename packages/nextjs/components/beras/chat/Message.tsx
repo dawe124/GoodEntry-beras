016444a062e10d5b0c4c1ae5c32706e3f6ea4d6e
Nicolas <lapingarou@gmail.com>
2024-05-13 15:25:41 +0800

    fix: username bright colors

--- packages/nextjs/components/beras/chat/Message.tsx
+++ packages/nextjs/components/beras/chat/Message.tsx
@@ -11,25 +11,32 @@ export const Message = ({
   date: number;
   isMe: boolean;
 }) => {
-  const stringToColour = (str: string) => {
+  const stringToHsl = (str: string) => {
+    // bright colors
     let hash = 0;
     str.split("").forEach(char => {
       hash = char.charCodeAt(0) + ((hash << 5) - hash);
     });
-    let colour = "#";
-    for (let i = 0; i < 3; i++) {
-      const value = (hash >> (i * 8)) & 0xff;
-      colour += value.toString(16).padStart(2, "0");
-    }
-    return colour;
-  };
+    const hue = Math.floor((hash % 341) * 341); // between 0 and 340
+    let saturation = 100;
+    let lightness = 50;
 
+    // blue color adjustment:
+    if (hue > 215 && hue < 265) {
+      const gain = 20;
+      const blueness = 1 - Math.abs(hue - 240) / 25;
+      const change = Math.floor(gain * blueness);
+      lightness += change;
+      saturation -= change;
+    }
+    return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
+  };
+  const color = isMe ? "red" : stringToHsl(username);
   const d = new Date(date);
-  const color = isMe ? "red" : stringToColour(username);
 
   return (
     <div className="bg-base-100 hover:bg-base-200 w-full p-1 rounded-md duration-100 ">
-      <span className="text-accent">
+      <span className="text-accent text-xs">
         [{String(d.getHours()).padStart(2, "0")}:{String(d.getMinutes()).padStart(2, "0")}]{" "}
       </span>
       <span style={{ color: color, fontWeight: "bold" }}>{username}</span>{" "}
