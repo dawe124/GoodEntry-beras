41e7cb03466d7b79907a6b876b81b1801638ee68
dawe124 <kovacs.david.istvan@gmail.com>
2024-05-14 09:13:07 +0700

    add token card details && fix swap card UI

--- packages/nextjs/next.config.js
+++ packages/nextjs/next.config.js
@@ -9,6 +9,10 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
   },
+  images: {
+    domains: ['https://blush-genuine-alpaca-303.mypinata.cloud'],
+    unoptimized: true
+  },
   webpack: config => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     config.externals.push("pino-pretty", "lokijs", "encoding");
