eec9643b85d52dcab446df4a272d85773f166722
Nicolas <lapingarou@gmail.com>
2024-05-16 13:31:38 +0800

    feat: lottery

--- packages/foundry/test/TokenController.t.sol
+++ packages/foundry/test/TokenController.t.sol
@@ -4,20 +4,9 @@ pragma solidity 0.8.19;
 import "forge-std/Test.sol";
 import "../contracts/TokenController.sol";
 
-contract TokenControllerTest is Test {
+contract TokenController_Test is Test {
   TokenController public tokenController;
-  address internal USDC = 0x6581e59A1C8dA66eD0D313a0d4029DcE2F746Cc5;
-  address internal MUCH_USDC = 0x8a73D1380345942F1cb32541F1b19C40D8e6C94B;
-  address internal HONEY = 0x7EeCA4205fF31f947EdBd49195a7A88E6A91161B;
-  address internal bHONEY = 0x95c5B81Fb99c91D4EAE15a28302c0200607b9D4e;
-  address internal MUCH_HONEY = 0xc63FAb87Cb00249190577937Ab6E04dA0AE69633;
-  
-  address internal MIM = 0xB734c264F83E39Ef6EC200F99550779998cC812d;
-  address internal MUCH_MIM = 0x34504A1E7b9161c8C615f9c5F023312bBA030354;
-  
-  address internal MUCH_BERA = 0x5806E416dA447b267cEA759358cF22Cc41FAE80F;
-  
-  address internal token;
+
   
   fallback() external payable {}
   receive() external payable {}
@@ -25,35 +14,68 @@ contract TokenControllerTest is Test {
 
   function setUp() public {
     tokenController = new TokenController();
-    // give ourselves some eth
-    console.log('erthb', address(this).balance);
   }
   
   
   function test_Creation_NoBuy() public {
     string memory name = "Las Beras";
-    token = tokenController.createToken(name, name, '{"img":"QmPjxUWe9X8fpZWtr3YaWvb4x5iEBw3MBVXv46CWJCumVM","desc":"Dummy desc"}');
+    assertEq(tokenController.getTokensLength(), 0);
+    (address token, ) = tokenController.createToken(name, name, '{"img":"QmPjxUWe9X8fpZWtr3YaWvb4x5iEBw3MBVXv46CWJCumVM","desc":"Dummy desc"}');
+    assertEq(tokenController.getTokensLength(), 1);
     assertEq(ERC20(token).name(), name);
     assertEq(ERC20(token).symbol(), name);
+    
+    address[] memory lt = tokenController.getLastTokens();
+    assertEq(lt.length, 1);
+    assertEq(lt[0], token);
+    
+    vm.expectRevert("Create: Already registered");
+    tokenController.createToken(name, name, '{"img":"QmPjxUWe9X8fpZWtr3YaWvb4x5iEBw3MBVXv46CWJCumVM","desc":"Dummy desc"}');
   }
   
   
-  function test_BuySell() public {
-    string memory name = "Las Beras";
-
-    token = tokenController.createToken(name, name, '{"img":"QmPjxUWe9X8fpZWtr3YaWvb4x5iEBw3MBVXv46CWJCumVM","desc":"Dummy desc"}');
-    uint bought = tokenController.buy{value: 1e18}(token, 0);
-    assertGt(bought, 0);
-    // balance should be 1e18 - 1e15 fee
-    assertEq(address(tokenController).balance, 1e18 - 1e15);
+  function testfuzz_DepositJackpot(uint amount) public {
+    vm.assume(amount < 1e10 ether);
+    tokenController.depositJackpot{value: amount}();
+    assertEq(tokenController.dailyJackpot(tokenController.today()+1), amount);
+  }
+  
+  
+  //////////////// SETTERS
+  
+  function test_setTradingFee() public {
+    vm.expectRevert("Trading fee too high");
+    tokenController.setTradingFees(220, 80);
+    vm.expectRevert("Trading fee too high");
+    tokenController.setTradingFees(80, 270);
     
-    // sale should return a bit less than the current controller balance (99.9% with default fee of 0.1%)
-    assertEq(tokenController.getAmountSale(token, bought), address(tokenController).balance * 999 / 1000);
+    tokenController.setTradingFees(80, 50);
+    assertEq(tokenController.tradingFee(), 80);
+    assertEq(tokenController.treasuryFee(), 50);
+  }
+  
+  function test_setTreasury() public {
+    vm.expectRevert("Invalid treasury");
+    tokenController.setTreasury(address(0));
+    tokenController.setTreasury(address(123));
+    assertEq(tokenController.treasury(), address(123));
+  }
+  
+  function test_setSlope() public {
+    vm.expectRevert("Invalid slope");
+    tokenController.setSlope(1e17);
+    tokenController.setSlope(1e20);
+    assertEq(tokenController.slope(), 1e20);
+  }
+  
+  function test_setMcapToAmm() public {
+    vm.expectRevert("Invalid Mcap");
+    tokenController.setMcapToAmm(10e18);
+    vm.expectRevert("Invalid Mcap");
+    tokenController.setMcapToAmm(100_000_000e18);
     
-    vm.expectRevert("ERC20: insufficient allowance");
-    tokenController.sell(token, bought / 2);
-    ERC20(token).approve(address(tokenController), type(uint).max);
-    uint sold = tokenController.sell(token, bought / 2);
-    console.log("sold, ", sold);
+    tokenController.setMcapToAmm(100_000e18);
+    assertEq(tokenController.mcapToAmm(), 100_000e18);
+    
   }
 }
