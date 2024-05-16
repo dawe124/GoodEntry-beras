909fe00cd1b11c9fb76c8448d9de9a7c9741889b
Nicolas <lapingarou@gmail.com>
2024-05-16 16:09:54 +0800

    fix: faucet link when on berachain

--- packages/nextjs/components/scaffold-eth/FaucetButton.tsx
+++ packages/nextjs/components/scaffold-eth/FaucetButton.tsx
@@ -11,8 +11,8 @@ import { useTransactor } from "~~/hooks/scaffold-eth";
 import { useTargetNetwork } from "~~/hooks/scaffold-eth/useTargetNetwork";
 
 // Number of ETH faucet sends to an address
-const NUM_OF_ETH = "0.1";
-const FAUCET_ADDRESS = "0xa3173a8757aFE9Da7758e610Df6ed4F8198FA053";
+const NUM_OF_ETH = "1";
+const FAUCET_ADDRESS = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
 
 const localWalletClient = createWalletClient({
   chain: hardhat,
@@ -57,33 +57,35 @@ export const FaucetButton = () => {
     }
   };
 
-  // // Render only on local chain
-  // if (ConnectedChain?.id !== hardhat.id) {
-  //   return null;
-  // }
+  // Render only on local chain
+  if (ConnectedChain?.id !== hardhat.id) {
+    return (
+      <a href="https://artio.faucet.berachain.com/" target="_blank">
+        <button className="btn btn-secondary btn-sm px-2 rounded-full mr-2">
+          <BanknotesIcon className="h-4 w-4" />
+        </button>
+      </a>
+    )
+  }
 
   const isBalanceZero = balance && balance.value === 0n;
 
   return (
-    <>
-      {ConnectedChain?.id == process.env.FAUCET_CHAIN_ID ? (
-        <div
-          className={
-            !isBalanceZero
-              ? "ml-1"
-              : "ml-1 tooltip tooltip-bottom tooltip-secondary tooltip-open font-bold before:left-auto before:transform-none before:content-[attr(data-tip)] before:right-0"
-          }
-          data-tip="Grab funds from faucet"
-        >
-          <button className="btn btn-secondary btn-sm px-2 rounded-full" onClick={sendETH} disabled={loading}>
-            {!loading ? (
-              <BanknotesIcon className="h-4 w-4 text-neutral" />
-            ) : (
-              <span className="loading loading-spinner loading-xs"></span>
-            )}
-          </button>
-        </div>
-      ) : null}
-    </>
+    <div
+      className={
+        !isBalanceZero
+          ? "ml-1"
+          : "ml-1 tooltip tooltip-bottom tooltip-secondary tooltip-open font-bold before:left-auto before:transform-none before:content-[attr(data-tip)] before:right-0"
+      }
+      data-tip="Grab funds from faucet"
+    >
+      <button className="btn btn-secondary btn-sm px-2 rounded-full" onClick={sendETH} disabled={loading}>
+        {!loading ? (
+          <BanknotesIcon className="h-4 w-4" />
+        ) : (
+          <span className="loading loading-spinner loading-xs"></span>
+        )}
+      </button>
+    </div>
   );
 };
