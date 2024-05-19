23406c732c7558373aff75f28882eedde3b894ff
dawe124 <kovacs.david.istvan@gmail.com>
2024-05-19 10:39:00 +0700

    add limit for chat input

--- packages/nextjs/components/beras/chat/ChatInput.tsx
+++ packages/nextjs/components/beras/chat/ChatInput.tsx
@@ -4,7 +4,10 @@ import { EmojisPopup } from "./EmojisPopup";
 
 export const ChatInput = ({ isDisabled, sendMessage }: { isDisabled: boolean; sendMessage: (msg: string) => void }) => {
   const [message, setMessage] = useState<string>("");
+  const [msgTooLongAlert, setMsgTooLongAlert] = useState<boolean>(false);
   const [isEmojisVisible, setEmojisVisible] = useState<boolean>(false);
+
+  const inputLimit = 160;
 
   const onSelectEmoji = (emoji: string) => {
     setMessage(message + ":" + emoji + ":");
@@ -22,6 +25,27 @@ export const ChatInput = ({ isDisabled, sendMessage }: { isDisabled: boolean; se
       )}
 
       <div className="md:absolute fixed bg-base-100 h-12 bottom-2 md:right-5 left-2 right-2 flex flex-row items-center rounded-md">
+        {msgTooLongAlert && (
+          <div className="absolute -top-24">
+            <div role="alert" className="alert alert-error flex flex-row rounded-[1rem]">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                onClick={() => setMsgTooLongAlert(false)}
+                className="text-neutral cursor-pointer stroke-current shrink-0 h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+              <span className="text-neutral">An average bera can&apos;t comprehend this much text.</span>
+            </div>
+          </div>
+        )}
         <div className="relative hover:animate-custom-bounce">
           <Image
             src="/emojis/kek.png"
@@ -39,10 +63,16 @@ export const ChatInput = ({ isDisabled, sendMessage }: { isDisabled: boolean; se
           placeholder={isDisabled ? "Log in to chat" : "Type here"}
           disabled={isDisabled}
           className="input input-bordered border rounded-r-md  w-full text-neutral rounded-md shadow-md border-base-300 focus:outline-none focus:ring-2 focus:ring-accent"
-          maxLength={200}
+          maxLength={inputLimit}
           value={message}
           onChange={e => {
-            setMessage(e.target.value);
+            const prevMessage = message;
+            if (e.target.value.length < inputLimit) {
+              setMessage(e.target.value);
+            } else {
+              setMsgTooLongAlert(true);
+              setMessage(prevMessage);
+            }
           }}
           onKeyDown={e => {
             if (e.key === "Enter") {
